fed3d6c2ebaf0dbf8fb0ddcf3be7e71060f9b18e
cornellb28 <cornellb28@gmail.com>
2022-02-16 23:05:42 -0600

    created the interface 4 scan tags

--- buildts/main.js
+++ buildts/main.js
@@ -16,11 +16,11 @@ const createWindow = () => {
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
-            preload: path.join(__dirname, 'preload.js'),
+            preload: path.join(__dirname, "preload.js"),
         },
     });
     // and load the index.html of the app.
-    mainWindow.loadFile(path.join(__dirname, '../public/index.html'));
+    mainWindow.loadFile(path.join(__dirname, "../public/index.html"));
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
 };
@@ -29,7 +29,7 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 electron_1.app.whenReady().then(() => {
     createWindow();
-    electron_1.app.on('activate', () => {
+    electron_1.app.on("activate", () => {
         // On macOS it's common to re-create a window in the app when the
         // dock icon is clicked and there are no other windows open.
         if (electron_1.BrowserWindow.getAllWindows().length === 0)
@@ -39,23 +39,23 @@ electron_1.app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-electron_1.app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin')
+electron_1.app.on("window-all-closed", () => {
+    if (process.platform !== "darwin")
         electron_1.app.quit();
 });
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 // scan the directory for all files and folders
-electron_1.ipcMain.handle('upload-files', async (event) => {
+electron_1.ipcMain.handle("upload-files", async (event) => {
     // function to open dialog on click
     const dialogButton = await electron_1.dialog.showOpenDialog({
-        properties: ['openDirectory', 'createDirectory', 'openFile'],
+        properties: ["openDirectory", "createDirectory", "openFile"],
     });
     // Scan the root directory user selected
     async function scanDirectory(filepath) {
         //console.log(filepath[0]) returns etc "/Volumes/MUSICLITE/CAPITALRECORDS/90s"
         return new Promise((resolve, reject) => {
-            glob('/**/*.mp3', { root: filepath[0] }, (err, files) => {
+            glob("/**/*.mp3", { root: filepath[0] }, (err, files) => {
                 try {
                     resolve(files);
                 }
@@ -72,14 +72,15 @@ electron_1.ipcMain.handle('upload-files', async (event) => {
     const rootDir = dialogButton.filePaths; // Selected directory path
     const audioFiles = await scanDirectory(rootDir); // returns selected directory files (scanned)
     // empty array to send selection
-    const songs = [];
+    // const songs: TrackData {
+    // };
     for (let audioPath of audioFiles.slice(0, 50)) {
         // set a promise that resolve the promise if tags exist //
         const tags = await new Promise((resolve, reject) => {
             NodeID3.read(audioPath, { noRaw: true }, (err, tags) => {
                 // early exit
                 if (err) {
-                    console.log('nodeid3 issue', err);
+                    console.log("nodeid3 issue", err);
                     reject(err);
                 }
                 resolve(tags);
@@ -88,15 +89,14 @@ electron_1.ipcMain.handle('upload-files', async (event) => {
         // early exit
         if (tags === null)
             continue; // tags does not exist move on to the next audio file
-        console.log(tags);
         let audioObject = {
             ...tags,
         };
         const { image, comment } = audioObject;
-        if (typeof image === 'undefined' ||
-            typeof image === 'string' ||
-            typeof comment === 'undefined' ||
-            typeof comment === 'string')
+        if (typeof image === "undefined" ||
+            typeof image === "string" ||
+            typeof comment === "undefined" ||
+            typeof comment === "string")
             continue;
         const { text } = comment;
         const { imageBuffer } = image;
@@ -107,12 +107,12 @@ electron_1.ipcMain.handle('upload-files', async (event) => {
         // await dataImage.toFile(`..covers/record-${id}.${dataMime}`)
         const data = JSON.stringify(audioObject);
         const coverBuffer = await dataImageLarge.jpeg({ quality: 100 }).toBuffer();
-        const updatedFile = _.omit(audioObject, ['image', 'comment']);
+        const updatedFile = _.omit(audioObject, ["image", "comment"]);
         // songs.push({
         //   cover: `data:image/jpeg;base64,` + coverBuffer.toString('base64'),
         //   ...updatedFile,
         // })
     }
-    return songs;
+    //return songs;
 });
 //# sourceMappingURL=main.js.map
--- electrondev/main.ts
+++ electrondev/main.ts
@@ -1,138 +1,168 @@
 // Modules to control application life and create native browser window
-import { BrowserWindow, app, ipcMain, dialog } from 'electron'
-import * as path from 'path'
-import * as glob from 'glob'
-import * as NodeID3 from 'node-id3'
-import * as sharp from 'sharp'
-import * as uniqid from 'uniqid'
-import * as _ from 'lodash'
+import { BrowserWindow, app, ipcMain, dialog } from "electron";
+import * as path from "path";
+import * as glob from "glob";
+import * as NodeID3 from "node-id3";
+import * as sharp from "sharp";
+import * as uniqid from "uniqid";
+import * as _ from "lodash";
 
 const createWindow = () => {
   // Create the browser window.
-  console.log(__dirname)
+  console.log(__dirname);
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
-  })
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, '../public/index.html'))
+  mainWindow.loadFile(path.join(__dirname, "../public/index.html"));
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
-}
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
 // scan the directory for all files and folders
 
-ipcMain.handle('upload-files', async (event) => {
+ipcMain.handle("upload-files", async (event) => {
   // function to open dialog on click
   const dialogButton = await dialog.showOpenDialog({
-    properties: ['openDirectory', 'createDirectory', 'openFile'],
-  })
+    properties: ["openDirectory", "createDirectory", "openFile"],
+  });
 
   // Scan the root directory user selected
   async function scanDirectory(filepath: string[]) {
     //console.log(filepath[0]) returns etc "/Volumes/MUSICLITE/CAPITALRECORDS/90s"
     return new Promise<string[]>((resolve, reject) => {
-      glob('/**/*.mp3', { root: filepath[0] }, (err, files) => {
+      glob("/**/*.mp3", { root: filepath[0] }, (err, files) => {
         try {
-          resolve(files)
+          resolve(files);
         } catch (error) {
-          reject(error)
+          reject(error);
         }
-      })
-    })
+      });
+    });
   }
 
   // user selected cancel button
-  if (dialogButton.canceled) return
+  if (dialogButton.canceled) return;
 
   // User selected a filepath. continue -->
-  const rootDir = dialogButton.filePaths // Selected directory path
-  const audioFiles = await scanDirectory(rootDir) // returns selected directory files (scanned)
+  const rootDir = dialogButton.filePaths; // Selected directory path
+  const audioFiles = await scanDirectory(rootDir); // returns selected directory files (scanned)
+
+  // create an interface to show object model
+  type Image = { mime: string; imageBuffer: Buffer };
+  type UserDefinedtext = { description: string; value: string };
+  type Artists = { performerInfo: string; features: string[] };
+  type TrackTitle = { name: string; version: string };
+  type Comment = {
+    language: string;
+    shortText: undefined | string;
+    text: string[];
+  };
+  type Ratings = { ratings: number };
+  type Samples = { sample_artist: string; sample_title: string };
+
+  interface TrackData {
+    title: TrackTitle;
+    artist: Artists;
+    genre: string[];
+    contentGroup: string[];
+    year: string;
+    initialKey: string;
+    userDefinedText: UserDefinedtext[];
+    bpm: string;
+    image: Image;
+    publisher: string[];
+    comment: Comment;
+    composer: string[];
+    remixArtist: string[];
+    album: string;
+    length: string;
+    popularimeter: Ratings;
+    favorite: boolean;
+    SampleInfo: Samples;
+    artistUrl: string[];
+  }
 
   // empty array to send selection
-  const songs: {
-    cover: string
-    location: string
-    trackId: string
-    folderPath: string
-    tags: NodeID3.Tags
-    commentText: string
-  }[] = []
+  // const songs: TrackData {
+
+  // };
+
   for (let audioPath of audioFiles.slice(0, 50)) {
     // set a promise that resolve the promise if tags exist //
     const tags = await new Promise<NodeID3.Tags | null>((resolve, reject) => {
       NodeID3.read(audioPath, { noRaw: true }, (err, tags) => {
         // early exit
         if (err) {
-          console.log('nodeid3 issue', err)
-          reject(err)
+          console.log("nodeid3 issue", err);
+          reject(err);
         }
-        resolve(tags)
-      })
-    })
+        resolve(tags);
+      });
+    });
     // early exit
-    if (tags === null) continue // tags does not exist move on to the next audio file
-    console.log(tags)
+    if (tags === null) continue; // tags does not exist move on to the next audio file
 
     let audioObject = {
       ...tags,
-    }
-    const { image, comment } = audioObject
+    };
+    const { image, comment } = audioObject;
     if (
-      typeof image === 'undefined' ||
-      typeof image === 'string' ||
-      typeof comment === 'undefined' ||
-      typeof comment === 'string'
+      typeof image === "undefined" ||
+      typeof image === "string" ||
+      typeof comment === "undefined" ||
+      typeof comment === "string"
     )
-      continue
+      continue;
 
-    const { text } = comment
-    const { imageBuffer } = image
+    const { text } = comment;
+    const { imageBuffer } = image;
 
-    let dataImageLarge = sharp(Buffer.from(imageBuffer)).resize(100)
+    let dataImageLarge = sharp(Buffer.from(imageBuffer)).resize(100);
     // let dataImageMedium = sharp(Buffer.from(imageBuffer)).resize(50)
     // let dataImageSmall = sharp(Buffer.from(imageBuffer)).resize(35)
     // let dataMime = mime.split('/')[1].toLowerCase()
     // await dataImage.toFile(`..covers/record-${id}.${dataMime}`)
 
-    const data = JSON.stringify(audioObject)
+    const data = JSON.stringify(audioObject);
 
-    const coverBuffer = await dataImageLarge.jpeg({ quality: 100 }).toBuffer()
-    const updatedFile = _.omit(audioObject, ['image', 'comment'])
+    const coverBuffer = await dataImageLarge.jpeg({ quality: 100 }).toBuffer();
+    const updatedFile = _.omit(audioObject, ["image", "comment"]);
     // songs.push({
     //   cover: `data:image/jpeg;base64,` + coverBuffer.toString('base64'),
     //   ...updatedFile,
     // })
   }
-  return songs
-})
+  //return songs;
+});
